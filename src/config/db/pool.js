ef087134e06dcb6b76c2a56d2bbfad5acd2d4129
Opeoluwa7 <hazeofficial298@gmail.com>
2025-03-22 18:55:29 +0100

    minor fix

--- src/config/db/pool.js
+++ src/config/db/pool.js
@@ -1,19 +1,11 @@
 const env = require("../env.js");
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_NAME = process.env.DB_NAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_PORT = process.env.DB_PORT;
+const DB_URL = process.env.DB_URL;
 
 const { Pool } = require("pg");
 
 
 const pool = new Pool ({
-    host: DB_HOST,
-    user: DB_USER,
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: DB_PORT,
+    connectionString: DB_URL
 });
 
 module.exports = pool
