04c2dde0de7075647f57379aa6651212667677d2
Opeoluwa7 <hazeofficial298@gmail.com>
2025-03-29 12:09:58 +0100

    server

--- src/common/uploads.js
+++ src/common/uploads.js
@@ -1,14 +1,14 @@
 const multer = require("multer");
-const path = require("path");
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const uploadPath =  path.join(__dirname, "../../uploads");
-        console.log("Saving files to:", uploadPath)
+        const uploadPath =  "../../uploads/";
+        
         cb(null, uploadPath)
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname)
+        cb(null, file.originalname)
     }
 })
 
