6b7b9e44ec839aade2eac2b48c90d46e4e68d187
Opeoluwa7 <hazeofficial298@gmail.com>
2025-03-31 19:21:52 +0100

    final

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -4,10 +4,13 @@ const AuthController = require("../controllers/auth_controller.js");
 
 const rateLimit = require("../common/rate_limiter.js");
 
+const isAuthenticated = require("../middlewares/is_authenticated.js")
+
 const registerLimit = rateLimit(10, 10, "Too many registeration attempts. Try again in 10 minutes.")
 const loginLimit = rateLimit(10, 10, "Too many login attempts!. Try again in 10 minutes.");
 
-router.post('/register', registerLimit, AuthController.register)
-router.post('/login', loginLimit, AuthController.login)
+router.post('/register', registerLimit, AuthController.register);
+router.post('/login', loginLimit, AuthController.login);
+router.post('/logout', [isAuthenticated.check], AuthController.logout);
 
 module.exports = router
